c2aed12cfe14039df22a1c2789538d9f3d98427d
O-Isaac <zaragozamendozaisaac@gmail.com>
2023-12-23 02:06:02 +0100

    feat(domain): change og to services

--- src/utils/domain.ts
+++ src/utils/domain.ts
@@ -1,11 +1,11 @@
 export function getHostname() {
     if (process.env.VERCEL_ENV === "production") {
-        return "og.proyectograndorder.es"
+        return "services.proyectograndorder.es"
     }
 
     if (process.env.NODE_ENV == "development" && !process.env.VERCEL_ENV) {
         return "localhost:3000"
     }
 
-    return process.env.VERCEL_URL || "og.proyectograndorder.es"
+    return process.env.VERCEL_URL || "services.proyectograndorder.es"
 }
